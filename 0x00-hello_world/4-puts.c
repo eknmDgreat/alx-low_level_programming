6cdab283124faf47f3ee593d6d7a0754502e47f8
kalenga <edwardkalengam@gmail.com>
2022-12-11 19:45:52 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
+#include <stdio.h>	
+
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/	
+ *main - main block 
+ *Return: 0 
+ */	
 
 int main(void)	
 {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);	
+}	
