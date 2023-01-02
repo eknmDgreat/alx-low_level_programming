1d2abf4a969c14bc0114851026c0a04362a288c8
Kalenga <edwardkalengam@gmail.com>
2023-01-02 15:12:07 +0200

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* _strchr - located a character in a string
+* @c: occurence of the character
+* @s: in the string
+* Return: a pointer to the first occurence of the character
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
