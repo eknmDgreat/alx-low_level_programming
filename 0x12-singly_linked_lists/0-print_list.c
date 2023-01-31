016acb56d0b3331c285e567ca0917b53e8dcf7b7
Kalenga <edwardkalengam@gmail.com>
2023-01-31 22:55:32 +0200

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,11 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-	@@ -23,8 +23,8 @@ size_t print_list(const list_t *h)
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0,  "(nil)");
+		}
+		else
 		{
 		printf("[%d] %s\n", h->len, h->str);
 		}
